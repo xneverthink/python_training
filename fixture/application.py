98749b81ebdea9f932adde8d6ca2b816661747f2
dinar <xneverthink@gmail.com>
2020-07-19 13:59:45 +0300

    Added command-line options (browser and baseUrl).

--- fixture/application.py
+++ fixture/application.py
@@ -2,21 +2,33 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
+from selenium.webdriver.chrome.options import Options
 
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            options = Options()
+            options.binary_location = "C:/Program Files (x86)/Google/Chrome/Application/chrom.exe"
+            self.wd = webdriver.Chrome(options=options)
+            #self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_homepage(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("pass")) > 0):
-            wd.get("http://localhost/addressbook/") \
+            wd.get(self.base_url) \
 
 
     def destroy(self):
